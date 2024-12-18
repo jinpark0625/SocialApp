d5240f8afb332816e4dd490b204fdc59048c2afb
jin park <jinpark0625@gmail.com>
2024-12-18 12:40:08 +0900

    feat: defin globlal ColorTypes

---
+++ types/global.d.ts
@@ -0,0 +1,13 @@
+export {};
+
+declare global {
+  type ColorTypes =
+    | "primary"
+    | "gray_600"
+    | "gray_500"
+    | "gray_400"
+    | "gray_300"
+    | "gray_200"
+    | "gray_100"
+    | "white";
+}
