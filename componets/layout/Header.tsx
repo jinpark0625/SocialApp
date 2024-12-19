da785c97ecb8b96ad2957faede478c3eaa4f6a3b
jin park <jinpark0625@gmail.com>
2024-12-19 18:15:56 +0900

    feat: add left, right, and title props to Header component

--- componets/layout/Header.tsx
+++ componets/layout/Header.tsx
@@ -1,11 +1,13 @@
+import { Platform, View, StyleSheet, StatusBar } from "react-native";
+import { Text } from "../common";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { router, usePathname, useSegments } from "expo-router";
 import { useTheme } from "@react-navigation/native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Platform, View, StyleSheet, StatusBar } from "react-native";
+import { HeaderProps } from "@/types";
 
-const Header = () => {
+const Header = ({ left, right, title, style }: HeaderProps) => {
   const top = useSafeAreaInsets().top;
   const theme = useTheme();
   //   const pathname = usePathname();
@@ -13,22 +15,37 @@ const Header = () => {
 
   return (
     <View
-      style={{
-        paddingTop: top,
-        height:
-          Platform.OS === "android"
-            ? 56 + (StatusBar.currentHeight || 0)
-            : 44 + top,
-        ...styles.container,
-      }}
+      style={[
+        {
+          paddingTop: top,
+          height:
+            Platform.OS === "android"
+              ? 56 + (StatusBar.currentHeight || 0)
+              : 44 + top,
+        },
+        styles.container,
+        style,
+      ]}
     >
-      <TouchableOpacity
-        onPress={() => router.back()}
-        hitSlop={8}
-        style={styles.leftButtonContainer}
-      >
-        <Ionicons name="chevron-back" size={24} color={theme.colors.text} />
-      </TouchableOpacity>
+      <View style={styles.leftButtonContainer}>
+        {left !== undefined ? (
+          left
+        ) : (
+          <TouchableOpacity
+            onPress={() => router.back()}
+            hitSlop={8}
+            style={[styles.leftButtonContainer, styles.buttonWidth]}
+          >
+            <Ionicons name="chevron-back" size={24} color={theme.colors.text} />
+          </TouchableOpacity>
+        )}
+      </View>
+
+      <Text variant="subheading" weight="semiBold">
+        {title}
+      </Text>
+
+      <View style={styles.buttonWidth}>{right}</View>
     </View>
   );
 };
@@ -37,8 +54,13 @@ export default Header;
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     paddingHorizontal: 20,
+  },
+  buttonWidth: {
+    width: 36,
   },
   leftButtonContainer: {
     marginLeft: -5,
