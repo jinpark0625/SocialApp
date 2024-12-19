4e58b64b4392a5ce06e3bdb1c4bf2c84a4d8769a
jin park <jinpark0625@gmail.com>
2024-12-19 14:49:25 +0900

    feat: add modalState atom

---
+++ atoms/modalState.ts
@@ -0,0 +1,18 @@
+// atoms/modalState.ts
+import { atom } from "recoil";
+import { ModalProps } from "@/types/components";
+
+interface ModalState {
+  isOpen: boolean;
+  props?: ModalProps;
+}
+
+const modalState = atom<ModalState>({
+  key: "modalState",
+  default: {
+    isOpen: false,
+    props: undefined,
+  },
+});
+
+export default modalState;
