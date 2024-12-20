40cf5aa84ad3fdb0ce0b0806f215e05a190ce063
jin park <jinpark0625@gmail.com>
2024-12-20 11:43:56 +0900

    feat: add Image and Avatar components

--- componets/common/index.ts
+++ componets/common/index.ts
@@ -1,4 +1,6 @@
+export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
 export { default as Text } from "./Text";
 export { default as TextInput } from "./TextInput";
 export { default as Modal } from "./Modal";
+export { default as Image } from "./Image";
