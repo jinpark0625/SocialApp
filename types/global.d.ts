eafeb6bc8588d6c85fc6e211e8ba18ee79a48078
jin park <jinpark0625@gmail.com>
2024-12-20 17:06:35 +0900

    feat: define global types

--- types/global.d.ts
+++ types/global.d.ts
@@ -10,4 +10,43 @@ declare global {
     | "gray_200"
     | "gray_100"
     | "white";
+
+  type User = {
+    id: number;
+    email: string;
+    password?: string;
+    nickname: string;
+    profileImg: string;
+  };
+
+  type Feed = {
+    id: number;
+    userId: number;
+    image: string;
+    text: string;
+    comments: Comment[];
+    createdAt: string;
+  };
+
+  type Comment = {
+    id: number;
+    userId: number;
+    text: string;
+    createdAt: string;
+  };
+
+  type ProfileData = {
+    nickname?: string;
+    profileImg?: string;
+  };
+
+  type FeedData = {
+    image: string;
+    text: string;
+  };
+
+  type MockDatabase = {
+    users: User[];
+    feeds: Feed[];
+  };
 }
