8c7b76afd2dc31ef6d9fd881ee3ea4b46242f4e9
jin park <jinpark0625@gmail.com>
2024-12-20 11:59:21 +0900

    feat: add AppLayout

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,32 @@
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@/hooks";
+import { Header } from "@/componets/layout";
+
+const AppLayout = () => {
+  const { session } = useAuth();
+
+  if (!session) {
+    return <Redirect href="/(auth)" />;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+          gestureEnabled: false,
+        }}
+      />
+      <Stack.Screen
+        name="feed/[id]"
+        options={{
+          header: () => <Header />,
+          gestureEnabled: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default AppLayout;
