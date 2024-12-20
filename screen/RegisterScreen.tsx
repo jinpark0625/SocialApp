c83558b8ab991043a078264ca80e70a87a66ce6a
jin park <jinpark0625@gmail.com>
2024-12-20 11:55:14 +0900

    feat: implement auth flow

--- screen/RegisterScreen.tsx
+++ screen/RegisterScreen.tsx
@@ -1,31 +1,57 @@
 import {
   View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
   TouchableWithoutFeedback,
   Keyboard,
-  StyleSheet,
 } from "react-native";
 import { Hero } from "@/componets/layout";
 import { AuthForm } from "@/componets/sections";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useAuth } from "@/hooks";
+import { router } from "expo-router";
 
 const RegisterScreen = () => {
+  const INPUT_ACCESSORY_ID = "nickname";
   const bottom = useSafeAreaInsets().bottom;
+  const { signIn } = useAuth();
+
+  const onSubmit = async () => {
+    await signIn();
+    router.replace("/(app)/(tabs)");
+  };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={{ paddingBottom: bottom, ...styles.container }}>
-        <Hero
-          content={`회원 가입하고${`\n`}새로운 경험을 시작하세요`}
-          style={{ paddingTop: 20 }}
-        />
-        <AuthForm isRegister />
-      </View>
-    </TouchableWithoutFeedback>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? undefined : "height"}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : -54}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ paddingBottom: bottom, ...styles.container }}>
+          <Hero
+            content={`회원 가입하고${`\n`}새로운 경험을 시작하세요`}
+            style={styles.hero}
+          />
+
+          <AuthForm
+            isRegister
+            onSubmit={onSubmit}
+            inputAccessoryId={INPUT_ACCESSORY_ID}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
 export default RegisterScreen;
 
 const styles = StyleSheet.create({
-  container: { paddingHorizontal: 20, flex: 1 },
+  container: { flex: 1 },
+  hero: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
 });
