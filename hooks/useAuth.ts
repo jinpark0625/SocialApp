15c1bcbab7071155f0e3c975b57fd4eaf72af962
jin park <jinpark0625@gmail.com>
2024-12-20 17:09:34 +0900

    feat: add comments

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -4,6 +4,7 @@ const useAuth = () => {
   const [{ session, isLoading }, setSession] = useStorageState("session");
 
   const signIn = async () => {
+    // 임시 토큰 xxx
     await setSession("xxx");
   };
 
