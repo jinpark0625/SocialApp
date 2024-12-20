2fe7647bd351ab2dd9d6acdd60d980d59272991b
jin park <jinpark0625@gmail.com>
2024-12-20 11:52:13 +0900

    feat: add components

--- componets/sections/index.ts
+++ componets/sections/index.ts
@@ -1,1 +1,6 @@
 export { default as AuthForm } from "./AuthForm";
+export { default as CommentModal } from "./CommentModal";
+export { default as CommentInput } from "./CommentInput";
+export { default as CommentItem } from "./CommentItem";
+export { default as FeedItem } from "./FeedItem";
+export { default as PostGridItem } from "./PostGridItem";
